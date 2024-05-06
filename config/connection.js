5534283c74f8ed8cabea426d0525aaa0c44196f7
blebeuf <bryanlebeuf@gmail.com>
2024-05-05 22:30:49 -0500

    update syntax

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,3 @@
-// //based on mongodb documentation and module 18 assignments
-// const { connect, connection } = require('mongoose');
-
-// // Updated with a valid connection string
-// const connectionString = 'mongodb://127.0.0.1:27017/mySocialNetworkAPI';
-
-// connect(connectionString, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
-
-// module.exports = connection;
-
 const { connect, connection } = require('mongoose');
 
 // Updated with a valid connection string
